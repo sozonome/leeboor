83e3b92ed8647f216b889bbd2919a969bde8877c
Agustinus Nathaniel <berserk.nate@gmail.com>
2023-09-19 00:48:26 +0700

    refactor: add commentary

--- src/pages/_utils.ts
+++ src/pages/_utils.ts
@@ -1,7 +1,23 @@
-import { differenceInDays } from "date-fns";
+import { differenceInDays, format } from "date-fns";
+import id from "date-fns/locale/id";
 
 export const inferHoliday = (holidayDate: Date | string) => {
   const remainingDays = differenceInDays(new Date(holidayDate), new Date());
+  const formattedDate = format(new Date(holidayDate), "eeee, dd MMMM yyyy", {
+    locale: id,
+  });
 
-  return { remainingDays };
+  const getCommentary = () => {
+    if (remainingDays > 3) {
+      return `Masih ${remainingDays} hari lagi nih...`;
+    }
+
+    if (remainingDays > 1) {
+      return `Wah, ${remainingDays} hari lagi!`;
+    }
+
+    return "Besok!";
+  };
+
+  return { remainingDays, formattedDate, commentary: getCommentary() };
 };
