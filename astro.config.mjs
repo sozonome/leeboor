06c7d0bbe9049ef7b737e91429f66c41a15c3a79
Agustinus Nathaniel <berserk.nate@gmail.com>
2023-09-18 20:58:31 +0700

    fix: adjust import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind({ applyBaseStyle: false })],
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyle: false,
+    }),
+  ],
+  output: "server",
+  adapter: vercel(),
 });
