b022bfdbcb19ee5057ca6e92ed9040586f2d69cf
Agustinus Nathaniel <berserk.nate@gmail.com>
2023-09-19 01:56:59 +0700

    refactor: adjust text

--- src/pages/_utils.ts
+++ src/pages/_utils.ts
@@ -5,9 +5,13 @@ import id from "date-fns/locale/id/index";
 
 export const inferHoliday = (holidayDate: Date | string) => {
   const remainingDays = differenceInDays(new Date(holidayDate), new Date());
-  const formattedDate = format(new Date(holidayDate), "eeee, dd MMMM yyyy", {
-    locale: id,
-  });
+  const formattedDate = formatDate(
+    new Date(holidayDate),
+    "eeee, dd MMMM yyyy",
+    {
+      locale: id,
+    }
+  );
 
   const getCommentary = () => {
     if (remainingDays > 3) {
@@ -23,3 +27,8 @@ export const inferHoliday = (holidayDate: Date | string) => {
 
   return { remainingDays, formattedDate, commentary: getCommentary() };
 };
+
+export const formatDate = (date: Date | string) =>
+  format(new Date(date), "eeee, dd MMMM yyyy", {
+    locale: id,
+  });
